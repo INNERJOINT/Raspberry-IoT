3aa1ad3e85e23504debaadcc2da4e17023f4b3d0
INNERJOINT <805401318@qq.com>
2020-10-13 18:35:23 +0800

    DHT11

---
+++ DHT11/dht11_test.py
@@ -0,0 +1,26 @@
+import time
+import dht11
+import RPi.GPIO as GPIO
+
+#define GPIO 14 as DHT11 data pin
+Temp_sensor=14
+def main():
+  # Main program block
+  GPIO.setwarnings(False)
+  GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
+  # Initialise display
+#  lcd_init()
+  instance = dht11.DHT11(pin = Temp_sensor)
+
+  while True:
+        #get DHT11 sensor value
+        result = instance.read()
+  print"Temperature = ",result.temperature,"C"," Humidity = ",result.humidity,"%"
+  time.sleep(1)
+
+if __name__ == '__main__':
+
+  try:
+    main()
+  except KeyboardInterrupt:
+    pass
